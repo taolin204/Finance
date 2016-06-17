7b0cbeb60041021a36f533dd85e28c6bbc428006
taolin204 <taolin204@qq.com>
2016-06-17 16:10:50 +0800

    20160618

---
+++ 东方财富/dailyFileCreation.bat
@@ -0,0 +1,23 @@
+rem copy common images
+echo f | xcopy /Y 0000.xls     沪深个股\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-DDE决策.xls
+echo f | xcopy /Y 0000.xls     沪深个股\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-财务数据.xls 
+echo f | xcopy /Y 0000.xls     沪深个股\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-行情列表.xls 
+echo f | xcopy /Y 0000.xls     沪深个股\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-盈利预测.xls 
+echo f | xcopy /Y 0000.xls     沪深个股\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-增仓排名.xls
+echo f | xcopy /Y 0000.xls     沪深个股\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-资金流向.xls  
+
+echo f | xcopy /Y 0000.xls     沪深板块\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-板块监测.xls
+echo f | xcopy /Y 0000.xls     沪深板块\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-增仓排名.xls
+echo f | xcopy /Y 0000.xls     沪深板块\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-资金流向.xls 
+echo f | xcopy /Y 0000.xls     沪深板块\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-DDE决策.xls
+
+echo f | xcopy /Y 0000.xls     L2大户室\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-顶级挂单.xls
+echo f | xcopy /Y 0000.xls     L2大户室\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-拖拉机单.xls
+echo f | xcopy /Y 0000.xls     L2大户室\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-强势狙击.xls
+echo f | xcopy /Y 0000.xls     L2大户室\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-主力踪迹.xls
+echo f | xcopy /Y 0000.xls     L2大户室\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-资金全景.xls
+echo f | xcopy /Y 0000.xls     L2大户室\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%-DDE全景.xls
+
+echo f | xcopy /Y 0000.xls     沪深指数\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%.xls  
+echo f | xcopy /Y 0000.xls     新股IPO\%date:~0,4%%date:~5,2%%date:~8,2%\%date:~0,4%%date:~5,2%%date:~8,2%.xls     
+ 
